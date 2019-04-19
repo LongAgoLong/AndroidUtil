2ab9d85e46637b024aabc5e31aeeaf6aa5258e77
LEO <448740656@qq.com>
2019-04-19 09:41:53 +0800

    Update AppStackManager.java

--- common/src/main/java/com/leo/commonutil/app/AppStackManager.java
+++ common/src/main/java/com/leo/commonutil/app/AppStackManager.java
@@ -166,6 +166,27 @@ public class AppStackManager {
         }
         mActivities.clear();
     }
+    
+    /**
+     * 判断某个activity是否在前台显示
+     */
+    public boolean isForeground(Activity activity) {
+        return isForeground(activity, activity.getClass().getName());
+    }
+
+    /**
+     * 判断某个界面是否在前台,返回true，为显示,否则不是
+     */
+    public boolean isForeground(Activity context, String className) {
+        if (context == null || TextUtils.isEmpty(className)) return false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> list = am.getRunningTasks(1);
+        if (list != null && list.size() > 0) {
+            ComponentName cpn = list.get(0).topActivity;
+            if (className.equals(cpn.getClassName())) return true;
+        }
+        return false;
+    }
 
     /**
      * 是否在栈中
