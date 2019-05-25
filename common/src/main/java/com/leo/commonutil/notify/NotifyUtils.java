e7371b3c596bd47eab0a58a697723c7347e2c07c
leo <448740656@qq.com>
2019-05-25 13:03:22 +0800

    调整

--- common/src/main/java/com/leo/commonutil/notify/NotifyUtils.java
+++ common/src/main/java/com/leo/commonutil/notify/NotifyUtils.java
@@ -10,6 +10,9 @@ import android.support.annotation.Nullable;
 import android.support.annotation.RawRes;
 import android.support.annotation.RequiresPermission;
 
+import com.leo.system.ContextHelp;
+import com.leo.system.LogUtil;
+
 import java.io.IOException;
 
 /**
@@ -19,49 +22,50 @@ import java.io.IOException;
  * 消息提醒
  */
 public final class NotifyUtils {
+    private static final String TAG = NotifyUtils.class.getSimpleName();
+
     private NotifyUtils() {
     }
 
     /**
-     * @param context      Context实例
      * @param milliseconds 震动时长 , 单位毫秒
      */
     @RequiresPermission(android.Manifest.permission.VIBRATE)
-    public static void vibrate(Context context, long milliseconds) {
-        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    public static void vibrate(long milliseconds) {
+        Vibrator vibrator = (Vibrator) ContextHelp.getContext().getSystemService(Context.VIBRATOR_SERVICE);
         if (null != vibrator) {
             vibrator.vibrate(milliseconds);// 参数是震动时间(long类型)
         }
     }
 
     /**
-     * @param context  Context实例
      * @param pattern  自定义震动模式 。数组中数字的含义依次是[静止时长，震动时长，静止时长，震动时长。。。]单位是毫秒
      * @param isRepeat true-> 反复震动，false-> 只震动一次
      */
     @RequiresPermission(android.Manifest.permission.VIBRATE)
-    public static void vibrate(Context context, long[] pattern, boolean isRepeat) {
-        Vibrator vibrator = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
+    public static void vibrate(long[] pattern, boolean isRepeat) {
+        Vibrator vibrator = (Vibrator) ContextHelp.getContext().getSystemService(Service.VIBRATOR_SERVICE);
         if (null != vibrator) {
             vibrator.vibrate(pattern, isRepeat ? 1 : -1);
         }
     }
 
-    public static void playBee(final Context context, @RawRes int mediaId, @Nullable OnCompleteListener listener) {
-        AudioManager audioService = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    public static void playBee(@RawRes int mediaId, @Nullable OnCompleteListener listener) {
+        AudioManager audioService = (AudioManager) ContextHelp.getContext()
+                .getSystemService(Context.AUDIO_SERVICE);
         if (null == audioService) {
             return;
         }
-        boolean shouldPlayBeep = true;
+        //检查当前是否是静音模式
         if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
-            shouldPlayBeep = false;//检查当前是否是静音模式
+            LogUtil.i(TAG, "静音模式");
+            return;
         }
-
         MediaPlayer mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mediaPlayer.setOnCompletionListener(player -> player.seekTo(0));
 
-        AssetFileDescriptor file = context.getResources().openRawResourceFd(mediaId);
+        AssetFileDescriptor file = ContextHelp.getContext().getResources().openRawResourceFd(mediaId);
         try {
             mediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
             file.close();
@@ -73,9 +77,7 @@ public final class NotifyUtils {
         if (null == mediaPlayer) {
             return;
         }
-        if (shouldPlayBeep) {
-            mediaPlayer.start();
-        }
+        mediaPlayer.start();
         mediaPlayer.setOnCompletionListener(mp -> {
             mp.stop();
             if (null != listener) {
@@ -85,10 +87,10 @@ public final class NotifyUtils {
     }
 
     @RequiresPermission(android.Manifest.permission.VIBRATE)
-    public static void playBeeAndVibrate(final Context context, long milliseconds, @RawRes int mediaId, @Nullable OnCompleteListener listener) {
+    public static void playBeeAndVibrate(long milliseconds, @RawRes int mediaId, @Nullable OnCompleteListener listener) {
         //震动
-        vibrate(context, milliseconds);
+        vibrate(milliseconds);
         //提示音
-        playBee(context, mediaId, listener);
+        playBee(mediaId, listener);
     }
 }
