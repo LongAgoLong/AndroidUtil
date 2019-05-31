413631ad83adb790df60b3297aad1c3d97afcbe8
leo <448740656@qq.com>
2019-06-01 00:21:06 +0800

    调整

--- system/src/main/java/com/leo/system/DrawableUtil.java
+++ system/src/main/java/com/leo/system/DrawableUtil.java
@@ -6,18 +6,27 @@ import android.support.annotation.ColorRes;
 import android.support.v4.graphics.drawable.DrawableCompat;
 
 public class DrawableUtil {
-    /*
+
+    /**
      * 着色
-     * */
+     *
+     * @param context
+     * @param colorId
+     * @param drawable
+     * @return
+     */
     public static Drawable tint(Context context, @ColorRes int colorId, Drawable drawable) {
         Drawable wrapDrawable = DrawableCompat.wrap(drawable);
         DrawableCompat.setTint(wrapDrawable, context.getResources().getColor(colorId));
         return wrapDrawable;
     }
 
-    /*
+    /**
      * 恢复原来的颜色
-     * */
+     *
+     * @param drawable
+     * @return
+     */
     public static Drawable unTint(Drawable drawable) {
         Drawable unwrapDrawable = DrawableCompat.unwrap(drawable);
         DrawableCompat.setTintList(unwrapDrawable, null);
