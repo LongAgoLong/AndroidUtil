561f8332923f7156d1952a536ee292eadfcc4649
leo <448740656@qq.com>
2019-06-17 22:49:48 +0800

    数据库工具类新增转化回调注入

--- system/src/main/java/com/leo/system/DBExcuteHelp.java
+++ system/src/main/java/com/leo/system/DBExcuteHelp.java
@@ -4,7 +4,6 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
