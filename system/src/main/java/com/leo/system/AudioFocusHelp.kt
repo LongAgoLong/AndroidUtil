792fcf8fe4d8c0c81aa74b461346d3475dabbb5b
leo <448740656@qq.com>
2020-02-22 15:47:40 +0800

    更新获取焦点工具类

--- system/src/main/java/com/leo/system/AudioFocusHelp.kt
+++ system/src/main/java/com/leo/system/AudioFocusHelp.kt
@@ -3,9 +3,16 @@
  */
 package com.leo.system
 
+import android.annotation.TargetApi
 import android.content.Context
+import android.media.AudioFocusRequest
 import android.media.AudioManager
 import android.media.AudioManager.OnAudioFocusChangeListener
+import android.os.Build
+import androidx.annotation.NonNull
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+
 
 /**
  * 音频焦点辅助类
@@ -22,15 +29,66 @@ class AudioFocusHelp private constructor() : OnAudioFocusChangeListener {
         }
     }
 
-    fun requestAudioFocus(): Int {
+    /**
+     * @param listener：申请成功后对AudioFocus的监听；
+     * @param streamType：音频类型
+     * STREAM_ALARM：手机闹铃，STREAM_MUSIC：手机音乐
+     * STREAM_RING：电话铃声，STREAM_SYSTEAM：手机系统
+     * STREAM_DTMF：音调，STREAM_NOTIFICATION：系统提示
+     * STREAM_VOICE_CALL：语音电话
+     * @param durationHint：
+     * AudioManager.AUDIOFOCUS_GAIN:
+     * 长时间获得AudioFocus
+     * AudioManager.AUDIOFOCUS_LOSS:
+     * 长时间失去了Audio Focus，需要停止Audio的播放，并且释放Media资源。为了避免再次自动获得AudioFocus而继续播放，不然突然冒出来的声音会让用户感觉莫名其妙，直接放弃AudioFocus，如果需要再次播放，用户要在界面上点击开始播放，才重新初始化Media，进行播放。
+     * AudioManager.AUDIOFOCUS_LOSS_TRANSIENT：
+     * 暂时失去AudioFocus，并会很快再次获得。必须停止Audio的播放，但是因为是暂时失去AudioFocus，可以不释放Media资源；
+     * AUDIOFOCUS_GAIN_TRANSIENT:
+     * 暂时获取焦点 适用于短暂的音频
+     * AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK:
+     * 应用跟其他应用共用焦点但播放的时候其他音频会降低音量
+     */
+    @JvmOverloads
+    fun requestAudioFocus(listener: OnAudioFocusChangeListener = this,
+                          streamType: Int = AUDIO_STREAM_TYPE,
+                          durationHint: Int = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT): Int {
         mAudioManager ?: return AudioManager.AUDIOFOCUS_REQUEST_FAILED
-        return if (AudioManager.AUDIOFOCUS_REQUEST_GRANTED == mAudioManager!!.requestAudioFocus(this,
-                        AUDIO_STREAM_TYPE, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)) {
+        return if (AudioManager.AUDIOFOCUS_REQUEST_GRANTED == mAudioManager!!.requestAudioFocus(listener,
+                        streamType, durationHint)) {
             audioFocus = AudioManager.AUDIOFOCUS_GAIN
             AudioManager.AUDIOFOCUS_REQUEST_GRANTED
         } else {
             AudioManager.AUDIOFOCUS_REQUEST_FAILED
         }
+    }
+
+    /**
+     * @return
+     * AudioManager.AUDIOFOCUS_REQUEST_FAILED -> 获取焦点失败：mPlaybackDelayed = false
+     * AudioManager.AUDIOFOCUS_REQUEST_GRANTED -> 获取焦点成功：mPlaybackDelayed = false
+     * AudioManager.AUDIOFOCUS_REQUEST_DELAYED -> 获取延迟焦点成功：mPlaybackDelayed = true
+     *
+     * {@link #https://developer.android.google.cn/reference/android/media/AudioFocusRequest}
+     */
+    @TargetApi(Build.VERSION_CODES.O)
+    fun requestAudioFocusO(@NonNull mediaPlayer: MediaPlayer,
+                           listener: OnAudioFocusChangeListener,
+                           usage: Int = AudioAttributes.USAGE_MEDIA,
+                           contentType: Int = AudioAttributes.CONTENT_TYPE_MUSIC,
+                           durationHint: Int = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT): Int {
+        mAudioManager ?: return AudioManager.AUDIOFOCUS_REQUEST_FAILED
+        val mPlaybackAttributes = AudioAttributes.Builder()
+                .setUsage(usage)
+                .setContentType(contentType)
+                .build()
+        val mFocusRequest = AudioFocusRequest.Builder(durationHint)
+                .setAudioAttributes(mPlaybackAttributes)
+                .setAcceptsDelayedFocusGain(true)
+                .setWillPauseWhenDucked(false)
+                .setOnAudioFocusChangeListener(listener)
+                .build()
+        mediaPlayer.setAudioAttributes(mPlaybackAttributes)
+        return mAudioManager!!.requestAudioFocus(mFocusRequest)
     }
 
     fun abandonAudioFocus(): Int {
@@ -42,18 +100,16 @@ class AudioFocusHelp private constructor() : OnAudioFocusChangeListener {
         }
     }
 
-    fun setAudioStreamType(type: Int) {
-        AUDIO_STREAM_TYPE = type
-    }
-
     fun registerAudioFocusListener(mTtsAudioFocusListener: OnAudioFocusChangeListener) {
         mRegisterAudioFocusListener = mTtsAudioFocusListener
     }
 
+    fun unregisterAudioFocusListener() {
+        mRegisterAudioFocusListener = null
+    }
+
     override fun onAudioFocusChange(focusChange: Int) {
-        if (mRegisterAudioFocusListener != null) {
-            mRegisterAudioFocusListener!!.onAudioFocusChange(focusChange)
-        }
+        mRegisterAudioFocusListener?.onAudioFocusChange(focusChange)
         audioFocus = focusChange
         when (focusChange) {
             AudioManager.AUDIOFOCUS_GAIN -> LogUtil.i(TAG, "focusChange：AUDIOFOCUS_GAIN")
@@ -67,6 +123,7 @@ class AudioFocusHelp private constructor() : OnAudioFocusChangeListener {
 
     companion object {
         private val TAG: String = AudioFocusHelp::class.java.simpleName
+        val mFocusLock = Any()
         var AUDIO_STREAM_TYPE = AudioManager.STREAM_MUSIC
         private var mInstance: AudioFocusHelp? = null
 
